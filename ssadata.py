757648e2f1e25c0a5d10ceb6172173899f0fffa6
Benjamin Mako Hill <mako@atdot.cc>
2023-04-10 15:54:37 -0700

    changed it to build dataset with a few more variables

--- ssadata.py
+++ ssadata.py
@@ -1,13 +1,34 @@
-NAMES_LIST = "yob2018.txt"
+import glob
+import re
 
-boys = {}
-girls = {}
+def import_yob_file(filename):
+    boys = {}
+    girls = {}
 
-for line in open(NAMES_LIST, 'r').readlines():
-    name, gender, count = line.strip().split(",")
-    count = int(count)
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            name, gender, count = line.strip().split(",")
+            count = int(count)
+            if gender == "F":
+                girls[name.lower()] = count
+            elif gender == "M":
+                boys[name.lower()] = count
+                
+    return((boys, girls))
 
-    if gender == "F":
-        girls[name.lower()] = count
-    elif gender == "M":
-        boys[name.lower()] = count
+years = {}
+for filename in glob.glob('yob*.txt'):
+    year = re.match(r'yob(\d{4})\.txt$', filename).group(1)
+    tmp_boys, tmp_girls = import_yob_file(filename)
+    years[year] = {'girls' : tmp_girls,
+                   'boys' : tmp_boys}
+
+## resort by year
+years = dict(sorted(years.items()))
+
+girls = years["2021"]["girls"]
+boys = years["2021"]["boys"]
+
+
+    
+    
